658852404dce457741238d0e6c2f4155d9f6c31c
Seu Nome <seuemail@example.com>
2025-09-02 16:32:36 -0300

    Anotações adicionadas de Contrutores, getters e setters

--- src/main/java/com/projeto/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/com/projeto/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,16 @@ package com.projeto.CadastroDeNinjas.Missoes;
 
 import com.projeto.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 
 public class MissoesModel {
 
@@ -20,27 +25,4 @@ public class MissoesModel {
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
 
-    public MissoesModel() {
-    }
-
-    public MissoesModel(String nome, String dificuldade) {
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
 }
--- src/main/java/com/projeto/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/com/projeto/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,9 +2,15 @@ package com.projeto.CadastroDeNinjas.Ninjas;
 
 import com.projeto.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 
 public class NinjaModel {
 
@@ -20,36 +26,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id") // Foreign key ou chave estrangeira
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
